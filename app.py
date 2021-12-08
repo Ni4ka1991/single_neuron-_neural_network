8f3e18714643f03da52cc491e086ebe94cddd558
Ni4ka1991 <astahova_nica@mail.ru>
2021-12-08 09:39:57 +0200

    Next stage

--- app.py
+++ app.py
@@ -10,51 +10,43 @@ plt.style .use( 'seaborn-whitegrid' )
 
 
 #experience
-#hours_a_day = [ 0,   4,   6,   8,   16, 20, 24 ]      #time elapsed to the projects per day (max 24 ours, min != 0)   x = hours_a_day = [0;24]
-#success     = [ 0,   100, 100, 95,  50, 20, 0  ]      # % syccessfully completed projects                             y = success = [0; 100]
 hours_a_day = [ 4,   6,   8,   16, 20 ]      #time elapsed to the projects per day (max 24 ours, min != 0)   x = hours_a_day = [0;24]
 success     = [ 100, 100, 95,  50, 20 ]      # % syccessfully completed projects                             y = success = [0; 100]
 
-#plt.plot( hours_a_day, success, color='green', linestyle = 'solid', linewidth = 1, marker = "x" )
+
+k = []
+
+for i in range( len( hours_a_day )):
+    k.append( success[i] / hours_a_day[i] )
+
+print(k)
+
+plt.plot( k,  color='red', linestyle = 'solid', linewidth = 1, marker = "x" )
 #plt.show( )
 
-## --------------------------------------------------------------------------------------###
-# График похож на параболу. Парабола - график квадратичной функции: y = a*x^2 + b*x + c
-# Ветви параболи направленны вниз, значит a < 0
-# При x = 0 => y = 0, значит с = 0
-# Получается: y = -a*x^2 + b*x
-# Корни уравнения: y = 0 при x = 0.001
-# 0 = -a*576 +24*b => b = 24a => y = a*x*( x + 24 )
-# a = y/( x^2 + 24*x )
-## --------------------------------------------------------------------------------------###
 
-#a = []
+###
+w = 0.5
+b = 0
+###
 
-#for i in range( len( hours_a_day )):
-#    a.append( success[i] / (hours_a_day[i] * hours_a_day[i] + 24 * hours_a_day[i] )) #error: Divizion by zero
+#1
+X = 4
+Y = ( X * k[0] ) * w # => yes syccess
 
-#print(a)
+#2
+X = 6
+Y = ( X * k[1] ) * w # => yes syccess
 
-#plt.plot( a,  color='red', linestyle = 'solid', linewidth = 1, marker = "x" )
-#plt.show( )
+#3
+X = 8
+Y = ( X * k[2] ) * w # => no syccess
 
-w = np.random.normal()
-#rint(w)
+#4
+X = 16
+Y = ( X * k[3] ) * w # => no syccess
 
-def sigmoid(x):
-    return 1 / (1 + np.exp( -1 ))
-
-class Neuron:
-    
-    def __init__( self, weight ):
-        self.weight = weight
-    
-    def feedForward( self, inputs ):
-        total = np.dot( self.weight, inputs )
-        return sigmoid( total )
-
-n = Neuron( w )
-print(n.feedForward( hours_a_day ))
-
-
+#5
+X = 20
+Y = ( X * k[4] ) * w # => no syccess
 
