f76677fd1d1b11655c80d8e53b3d92bdde7567ee
Ni4ka1991 <astahova_nica@mail.ru>
2021-12-08 10:22:05 +0200

    add def sigmoida

--- app.py
+++ app.py
@@ -9,36 +9,36 @@ plt.style .use( 'seaborn-whitegrid' )
 
 
 
-#experience
+#DATA
 hours_a_day = [ 4,   6,   8,   16, 20 ]      #time elapsed to the projects per day (max 24 ours, min != 0)   x = hours_a_day = [0;24]
 success     = [ 100, 100, 95,  50, 20 ]      # % syccessfully completed projects                             y = success = [0; 100]
+###
 
 
+#Dependencies
 k = []
 
 for i in range( len( hours_a_day )):
     k.append( success[i] / hours_a_day[i] )
-
-print(k)
-
-plt.plot( k,  color='red', linestyle = 'solid', linewidth = 1, marker = "x" )
-#plt.show( )
-
-# if k >= 16,6 => syccess
-# X < 4 ?????????????????   incomplete condition
-#syccess >= 80%
-
 ###
+
+
+#NEURON
+
 w = 0.5
 b = 0
 ###
 
-# YES/NO logic 1/0
+def sigmoida(var):
+    return 1 / ( 1 + np.exp(- var ))
 
+#view data
 for i in range( len( hours_a_day )):
     X = 4
-    Y = ( X * k[i] ) * w # => no syccess
+    Y = ( X * k[i] ) * w
     print( f"hours_a_day = {hours_a_day[i]:3};    syccess*w = {Y:20};    k = {k[i]:20};    w = 0.5" )
+
+
 
 #Upgrade the model with the activation function:
 
@@ -48,3 +48,6 @@ for i in range( len( hours_a_day )):
 
 
 
+# if k >= 16,6 => syccess
+# X < 4 ?????????????????   incomplete condition
+#syccess >= 80%
